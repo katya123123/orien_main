fdb9d0a59309a1b644dba0ccfa4386a508bcc613
calink <caplink777@gmail.com>
2023-12-15 01:37:11 +1000

    Message Update

--- opien/main/static/js/form.js
+++ opien/main/static/js/form.js
@@ -1,6 +1,21 @@
-function form() {
-    var name = document.getElementById("name").ariaValueText;
-    var email = document.getElementById("email").ariaValueText;
-    alert("Успешно отправлено")
-  }
+document.querySelector('button').addEventListener('click', _ => {
   
+  const om = document.querySelector('#sucs')
+  var stat = document.querySelector('#status')
+  var inputs = document.getElementsByClassName("form-control")
+  if ((inputs[0].value == '') || (inputs[1].value == '')){
+    stat.innerHTML = 'Не все поля заполнены'
+    om.style.display = 'block'
+    om.style.color = 'red'
+    return
+  }
+
+  om.style.display = 'block'
+  om.style.color = 'green'
+  stat.innerHTML = 'Ваша заявка успешно отправлена. Мы скоро с вами свяжемся!'
+  inputs[0].value = ''
+  inputs[1].value = ''
+  //om.addEventListener('click', _ => {
+  //  om.style.display = 'none'
+  //}, {once: true})
+})
