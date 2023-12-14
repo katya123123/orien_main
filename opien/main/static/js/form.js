83106c98a0bf351580446bf1eb9a4f920166d6e8
katya123123 <101809552+katya123123@users.noreply.github.com>
2023-12-14 19:25:22 +1000

    lil change

---
+++ opien/main/static/js/form.js
@@ -0,0 +1,6 @@
+function form() {
+    var name = document.getElementById("name").ariaValueText;
+    var email = document.getElementById("email").ariaValueText;
+    alert("Успешно отправлено")
+  }
+  
